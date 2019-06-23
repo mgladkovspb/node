ea265cb7909a01ce35d37009a5263a43f6aa847a
mgladkov <mgladkov@abz-1.ru>
2019-06-24 00:37:42 +0300

    HW2

--- HW2/app.js
+++ HW2/app.js
@@ -42,4 +42,45 @@ function task1() {
     console.log(gp.next(20).value);
 }
 
+function task3() {
+    class EventEmmiter {
+        constructor() {
+            this.events = {};
+        }
+
+        on(event, callback) {
+            if(this.events[event] === undefined)
+                this.events[event] = [];
+            this.events[event].push(callback);
+        }
+
+        emit(event, param) {
+            if(this.events[event] === undefined)
+                return;
+            this.events[event].forEach(callback => {
+                callback(param);
+            });
+        }
+    }
+
+    let em = new EventEmmiter();
+    em.on('eat', stringData => {
+        console.log('Первым: Я кушаю ' + stringData + '.');
+    });
+
+    em.on('eat', stringData => {
+        console.log('Вторым: Я кушаю ' + stringData + '.');
+    });
+
+    em.on('sleep', () => {
+        console.log('После вкусного обеда, нужно спать ;)');
+    });
+
+    setTimeout(() => { em.emit('eat', 'бутерброд'); }, 3000);
+    setTimeout(() => { em.emit('eat', 'мясо'); }, 2000);
+    setTimeout(() => { em.emit('eat', 'яблочко'); }, 500);
+    setTimeout(() => { em.emit('sleep'); }, 3500);
+}
+
 console.log('Задача №1. Результат работы:'), task1();
+console.log('Задача №3. Результат работы:'), task3();
